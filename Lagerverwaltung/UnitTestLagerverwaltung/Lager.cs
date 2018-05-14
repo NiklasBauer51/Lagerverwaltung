5c63c50e06425746fc7fdbbf8e730c80e9abe881
NiklasBauer51 <nbauer98@hotmail.de>
2018-05-14 12:55:49 +0200

    UnitTest1,2

---
+++ Lagerverwaltung/UnitTestLagerverwaltung/Lager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestLagerverwaltung
+{
+    class Lager
+    {
+        public Lager()
+        {
+            string Ort;
+            int Lagerbestand;
+            List<int> list = new List<int>();
+        }
+
+
+    }
+}
---
+++ Lagerverwaltung/UnitTestLagerverwaltung/LagerverwaltungTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Collections.Generic;
+
+namespace UnitTestLagerverwaltung
+{
+    [TestClass]
+    public class LagerverwaltungTest
+    {
+        Lager Lager1 = new Lager();
+        string Ort;
+        int BestandKoeln;
+        int BestandBonn;
+        int GesamtLagerbestand;
+        
+
+        [TestMethod]
+        public void Buchen()
+        {
+            GesamtLagerbestand = BestandBonn + BestandKoeln;
+            GesamtLagerbestand = GesamtLagerbestand - 500;
+            if(GesamtLagerbestand == 0)
+            {
+                BestandKoeln = 0;
+                BestandBonn = 0;
+            }
+
+        }
+
+        [TestMethod]
+        public void Umlagern()
+        {
+            BestandKoeln = 6000;
+            BestandBonn = 10000;
+            if (BestandBonn == 10000)
+            {
+
+            }
+
+        }
+
+    }
+}
